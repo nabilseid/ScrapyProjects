7c8f639b2bcf7d16b5ed9b679edce8c13eb56cdc
Nabil Seid <nabeelseid@gmail.com>
2020-10-14 13:46:46 +0300

    scrap title & countries' name

---
+++ worldometers/worldometers/spiders/countries.py
@@ -0,0 +1,14 @@
+import scrapy
+
+
+class CountriesSpider(scrapy.Spider):
+    name = 'countries'
+    allowed_domains = ['www.worldometers.info/']
+    start_urls = [
+        'https://www.worldometers.info/world-population/population-by-country/']
+
+    def parse(self, response):
+        title = response.css('div > h1::text').get()
+        countries = response.css('td > a::text').getall()
+
+        yield {'title': title, 'countries': countries}
