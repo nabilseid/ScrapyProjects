ee5c4db45edbf7dd73e23f520dc8554d64e7d4e8
Nabil Seid <nabeelseid@gmail.com>
2020-10-14 15:58:39 +0300

    P_worldometers spider gdp_debt DONE

--- worldometers/worldometers/spiders/gdp_debt.py
+++ worldometers/worldometers/spiders/gdp_debt.py
@@ -3,8 +3,16 @@ import scrapy
 
 class GdpDebtSpider(scrapy.Spider):
     name = 'gdp_debt'
-    allowed_domains = ['www.worldometers.info/world-population/population-by-country/']
-    start_urls = ['http://www.worldometers.info/world-population/population-by-country//']
+    allowed_domains = ['www.worldometers.info']
+    start_urls = [
+        'https://worldpopulationreview.com/countries/countries-by-national-debt']
 
     def parse(self, response):
-        pass
+        rows = response.xpath('//table/tbody/tr')
+
+        for row in rows:
+            name = row.xpath('.//td[1]/a/text()').get()
+            gdp_debt = row.xpath('.//td[2]/text()').get()
+            population = row.xpath('.//td[3]/text()').get()
+
+            yield {'country_name': name, 'gdp_debt': gdp_debt, 'population': population}
