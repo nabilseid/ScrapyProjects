38c3d2f11de6faae80b73cdfba277df89ddc7e4e
Nabil Seid <nabeelseid@gmail.com>
2020-10-15 17:58:26 +0300

    P_glassesshop spider bestsellers DONE

--- glassesshop/glassesshop/spiders/bestsellers.py
+++ glassesshop/glassesshop/spiders/bestsellers.py
@@ -3,8 +3,30 @@ import scrapy
 
 class BestsellersSpider(scrapy.Spider):
     name = 'bestsellers'
-    allowed_domains = ['www.glassesshop.com/bestsellers']
-    start_urls = ['http://www.glassesshop.com/bestsellers/']
+    # allowed_domains = ['www.glassesshop.com/bestsellers']
+    # start_urls = ['https://www.glassesshop.com/bestsellers']
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.glassesshop.com/bestsellers', callback=self.parse)
 
     def parse(self, response):
-        pass
+        products = response.xpath('//div[@id="product-lists"]/div')
+
+        for product in products:
+            product_content = product.xpath('.//div')
+
+            if len(product_content) < 2:
+                continue
+
+            yield {
+                'url': product_content[0].xpath('.//a/@href').get(),
+                'img_url': product_content[0].xpath('.//a/img[1]/@src').get(),
+                'name': product_content[1].xpath('.//div[@class="p-title"]/a/text()').get().strip(),
+                'price': product_content[1].xpath('.//div[@class="p-price"]/div/span/text()').get().strip(),
+            }
+
+        next_page = response.xpath(
+            '//li[@class="page-item"]/a[@rel="next"]/@href').get()
+        print(next_page)
+        if(next_page):
+            yield scrapy.Request(url=next_page, callback=self.parse)
