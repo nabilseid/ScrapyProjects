d3cfa0df725a14933561b88b1fb03d9de6910615
Nabil Seid <nabeelseid@gmail.com>
2020-10-17 01:29:07 +0300

    P_bookstoscrap crawlspider DONE

--- bookstoscrap/bookstoscrap/spiders/books.py
+++ bookstoscrap/bookstoscrap/spiders/books.py
@@ -6,15 +6,17 @@ from scrapy.spiders import CrawlSpider, Rule
 class BooksSpider(CrawlSpider):
     name = 'books'
     allowed_domains = ['books.toscrape.com']
-    start_urls = ['http://books.toscrape.com/']
+    start_urls = ['https://books.toscrape.com/']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(
+            restrict_xpaths='//div[@class="image_container"]/a'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths='//li[@class="next"]/a'))
     )
 
     def parse_item(self, response):
-        item = {}
-        #item['domain_id'] = response.xpath('//input[@id="sid"]/@value').get()
-        #item['name'] = response.xpath('//div[@id="name"]').get()
-        #item['description'] = response.xpath('//div[@id="description"]').get()
-        return item
+        yield {
+            'title': response.xpath('//div[@class="col-sm-6 product_main"]/h1/text()').get(),
+            'price': response.xpath('//p[@class="price_color"]/text()').get(),
+            'type': response.xpath('//ul[@class="breadcrumb"]/li[3]/a/text()').get(),
+        }
